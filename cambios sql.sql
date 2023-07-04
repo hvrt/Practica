19773ecc539ed950a91a524ccbaa95ef492fdeda
Hvrt <hvrtv13@gmail.com>
2023-07-03 18:41:42 -0600

    Scripts BD
    creacion y cambios de de BD

---
+++ cambios sql.sql
@@ -0,0 +1,27 @@
+use world_x;
+
+-- deshabilita el modo seguro 
+SET SQL_SAFE_UPDATES = 0;
+
+show tables;
+select * from City;
+select * from City where CountryCode = "MEX";
+update city
+	set District = "CDMX"
+    where ID = 2515;
+    -- where District = "Distrito Federal"
+select * from City where District = "Hidalgo";
+select * from City where CountryCode = "MEX" order by District Asc;
+insert into City (Name, CountryCode, District, Info)
+			values ("Omitlán", "MEX", "Hidalgo", "{\"Population\": 80500}");
+INSERT INTO `city` VALUES ('4081','Huasca','MEX','Hidalgo','{\"Population\": 105800}');
+update city
+	set Name = "Real del Monte"
+    where ID = 4080;
+update city
+	set Info = "{\"Population\": 50300}"
+    where ID = 4081;
+Delete from city where ID= 4081;
+
+show tables;
+select * from countryinfo;
